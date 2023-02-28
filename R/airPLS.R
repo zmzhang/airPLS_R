dd611c03830876ada7c4020f9fbe2a5046b07780
Matias Andina <matiasandina@gmail.com>
2023-02-28 10:42:01 -0500

    Adding :: to clarify masking of base R functions #1

--- R/airPLS.R
+++ R/airPLS.R
@@ -1,12 +1,12 @@
 WhittakerSmooth <- function(x,w,lambda,differences=1) {
   x=matrix(x,nrow = 1, ncol=length(x))
   L=length(x)
-  E=spMatrix(L,L,i=seq(1,L),j=seq(1,L),rep(1,L))
-  D=as(diff(E,1,differences),"dgCMatrix")
-  W=as(spMatrix(L,L,i=seq(1,L),j=seq(1,L),w),"dgCMatrix")
-  background=solve((W+lambda*t(D)%*%D),t((w*x)));
+  E=Matrix::spMatrix(L,L,i=seq(1,L),j=seq(1,L),rep(1,L))
+  D=as(Matrix::diff(E,1,differences),"dgCMatrix")
+  W=as(Matrix::spMatrix(L,L,i=seq(1,L),j=seq(1,L),w),"dgCMatrix")
+  background=Matrix::solve((W+lambda*Matrix::t(D)%*%D),Matrix::t((w*x)));
   return(as.vector(background))
- }
+}
  
 airPLS <- function(x,lambda=10,differences=1, itermax=20){
   
